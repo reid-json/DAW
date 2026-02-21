17320e244e42f3f70454db2bd32aa25663223f32
TDMartin66 <tdm70080@ucmo.edu>
2026-02-20 18:35:16 -0600

    Implemented JIVE properly (i think)

--- App/MainComponent.cpp
+++ App/MainComponent.cpp
@@ -1,9 +1,20 @@
-#include "MainComponent.h"
+#pragma once
 
+#include "MainComponent.h"
+#include <jive_layouts/jive_layouts.h>
+#include <jive_style_sheets/jive_style_sheets.h>
 //==============================================================================
+
 MainComponent::MainComponent()
 {
     setSize (600, 400);
+    // jive test
+    using namespace jive;
+    juce::Label testLabel;
+    addAndMakeVisible(testLabel);
+    testLabel.setText("JIVE works!", juce::dontSendNotification);
+    testLabel.setColour(juce::Label::backgroundColourId, juce::Colours::orange);
+    testLabel.setBounds(50, 50, 200, 50);
 }
 
 MainComponent::~MainComponent()
